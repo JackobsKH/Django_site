216fe9511c1b908af17e7516a6a44f1075b378c6
JackobsKH <jbkherson@gmail.com>
2022-07-25 11:40:36 +0300

    Added Category template

---
+++ mysite/news/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+# Create your models here.
+class News(models.Model):
+    title = models.CharField(max_length=150)
+    content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank='True')
+    is_published = models.BooleanField(default=True)
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'News'
+        verbose_name_plural = 'News'
+        ordering = ['-created_at', '-title']
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=150, db_index=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+        ordering = ['title']
