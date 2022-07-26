57488701a1124605a69b01c0f7bfe6362e99a549
JackobsKH <jbkherson@gmail.com>
2022-07-26 16:56:50 +0300

    "Added Get_absolute_url method in models News and Categories"

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.urls import reverse
+
 
 # Create your models here.
 class News(models.Model):
@@ -9,6 +11,9 @@ class News(models.Model):
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank='True')
     is_published = models.BooleanField(default=True)
     category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
+
+    def get_absolute_url(self):
+        return reverse('view_news', kwargs={"news_id": self.pk})
 
     def __str__(self):
         return self.title
@@ -21,6 +26,9 @@ class News(models.Model):
 
 class Category(models.Model):
     title = models.CharField(max_length=150, db_index=True)
+
+    def get_absolute_url(self):
+        return reverse('category', kwargs={"category_id": self.pk})
 
     def __str__(self):
         return self.title
