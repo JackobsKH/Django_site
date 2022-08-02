410bc16f3c44cd2c0e4177f309f9f52e288bb85b
JackobsKH <jbkherson@gmail.com>
2022-08-02 12:06:38 +0300

    Decorate form Add News via bootstrap

--- mysite/news/forms.py
+++ mysite/news/forms.py
@@ -2,7 +2,10 @@ from django import forms
 from .models import Category
 
 class NewsForm(forms.Form):
-    title = forms.CharField(max_length=150)
-    content = forms.CharField(max_length=150)
-    is_published = forms.BooleanField()
-    category = forms.ModelChoiceField(queryset=Category.objects.all())
+    title = forms.CharField(max_length=150, label="Title", widget=forms.TextInput(attrs={"class": "form-control"}))
+    content = forms.CharField(label="Content", widget=forms.Textarea(attrs={
+        "class": "form-control",
+        "rows": 8
+    }))
+    is_published = forms.BooleanField(initial=True, label="Is Published")
+    category = forms.ModelChoiceField(empty_label="Choose category", label="Category", queryset=Category.objects.all(), widget=forms.Select(attrs={"class": "form-control"}))
